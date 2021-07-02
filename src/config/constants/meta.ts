a3025177b00693b02c77dbe3a18f73e83f7753fe
panjule <panjule@yahoo.com>
2021-07-02 22:56:20 +0700

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -2,10 +2,10 @@ import { ContextApi } from 'contexts/Localization/types'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'PancakeSwap',
+  title: 'PIKASwap',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+    'Ready to 🌙⚡️MOONZAPP!!🌙⚡️ with us',
+  image: '',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
@@ -24,35 +24,35 @@ export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
       }
     case '/farms':
       return {
-        title: `${t('Farms')} | ${t('PancakeSwap')}`,
+        title: `${t('Farms')} | ${t('PIKASwap')}`,
       }
     case '/pools':
       return {
-        title: `${t('Pools')} | ${t('PancakeSwap')}`,
+        title: `${t('Pools')} | ${t('PIKASwap')}`,
       }
     case '/lottery':
       return {
-        title: `${t('Lottery')} | ${t('PancakeSwap')}`,
+        title: `${t('Lottery')} | ${t('PIKASwap')}`,
       }
     case '/collectibles':
       return {
-        title: `${t('Collectibles')} | ${t('PancakeSwap')}`,
+        title: `${t('Collectibles')} | ${t('PIKASwap')}`,
       }
     case '/ifo':
       return {
-        title: `${t('Initial Farm Offering')} | ${t('PancakeSwap')}`,
+        title: `${t('Initial Farm Offering')} | ${t('PIKASwap')}`,
       }
     case '/teams':
       return {
-        title: `${t('Leaderboard')} | ${t('PancakeSwap')}`,
+        title: `${t('Leaderboard')} | ${t('PIKASwap')}`,
       }
     case '/profile/tasks':
       return {
-        title: `${t('Task Center')} | ${t('PancakeSwap')}`,
+        title: `${t('Task Center')} | ${t('PIKASwap')}`,
       }
     case '/profile':
       return {
-        title: `${t('Your Profile')} | ${t('PancakeSwap')}`,
+        title: `${t('Your Profile')} | ${t('PIKASwap')}`,
       }
     default:
       return null
